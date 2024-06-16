5cbac4be32490bbb20e3b609f248fd849f4b8a7f
ENES YILMAZ <x14916@gmail.com>
2024-06-16 11:00:14 +0300

    fb

--- main.py
+++ main.py
@@ -1,201 +1,159 @@
 import pygame
-import pymunk
-import pymunk.pygame_util
-import math
+import random
+
+from objects import Birds, Pipe, Base, Score
+
 
 pygame.init()
+SCREEN = WIDTH, HEIGHT = 288, 512
+display_height = 0.80 * HEIGHT
+info = pygame.display.Info()
 
-SCREEN_WIDTH = 1200
-SCREEN_HEIGHT = 681  
-BOTTOM_PANEL = 50
+width = info.current_w
+height = info.current_h
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT + BOTTOM_PANEL))
-pygame.display.set_caption("Crazy Pool Game")
-
-space = pymunk.Space()
-static_body = space.static_body
-draw_options = pymunk.pygame_util.DrawOptions(screen)
+if width >= height:
+    win = pygame.display.set_mode(SCREEN, pygame.NOFRAME)
+else:
+    win = pygame.display.set_mode(SCREEN, pygame.NOFRAME | pygame.SCALED | pygame.FULLSCREEN)
 
 clock = pygame.time.Clock()
-FPS = 120
 
-lives = 3
-dia = 36
-pocket_dia = 66
-force = 0
-max_force = 10000
-force_direction = 1
-game_running = True
-cue_ball_potted = False
-taking_shot = True
-powering_up = False
-potted_balls = []
+FPS = 60
 
-BG = (40, 40, 40)
-GREEN = (0, 255, 50)
+
+RED = (255, 0, 0)
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 
-font = pygame.font.SysFont("Arial", 30)
-large_font = pygame.font.SysFont("Arial", 60)
 
-cue_image = pygame.image.load("images/cue.png").convert_alpha()
-table_image = pygame.image.load("images/table.png").convert_alpha()
-ball_images = [pygame.image.load(f"images/ball_{i}.png").convert_alpha() for i in range(1, 17)]
+try_again_img = pygame.image.load('Assets/try.png')
+try_again_img = pygame.transform.scale(try_again_img, (150, 50))
+try_again_rect = try_again_img.get_rect(center=(WIDTH // 2, HEIGHT * 0.8))
 
-def draw_text(text, font, text_col, x, y):
-    img = font.render(text, True, text_col)
-    screen.blit(img, (x, y))
 
-def create_ball(radius, pos):
-    body = pymunk.Body()
-    body.position = pos
-    shape = pymunk.Circle(body, radius)
-    shape.mass = 5
-    shape.elasticity = 0.8
+bg = pygame.image.load('Assets/bg/bg.png')
+pipe_img = pygame.image.load('Assets/bg/pipe.png')
 
-    pivot = pymunk.PivotJoint(static_body, body, (0, 0), (0, 0))
-    pivot.max_bias = 0
-    pivot.max_force = 1000
 
-    space.add(body, shape, pivot)
-    return shape
+gameover_img = pygame.image.load('Assets/bg/gameover.png')
+flappybird_img = pygame.image.load('Assets/bg/title.png')
+flappybird_img = pygame.transform.scale(flappybird_img, (200, 80))
 
-balls = []
-rows = 5
-for col in range(5):
-    for row in range(rows):
-        pos = (250 + (col * (dia + 1)), 267 + (row * (dia + 1)) + (col * dia / 2))
-        new_ball = create_ball(dia / 2, pos)
-        balls.append(new_ball)
-    rows -= 1
 
-pos = (888, SCREEN_HEIGHT / 2)
-cue_ball = create_ball(dia / 2, pos)
-balls.append(cue_ball)
+die_fx = pygame.mixer.Sound('Sounds/die.wav')
+hit_fx = pygame.mixer.Sound('Sounds/hit.wav')
+point_fx = pygame.mixer.Sound('Sounds/point.wav')
+swoosh_fx = pygame.mixer.Sound('Sounds/swoosh.wav')
+wing_fx = pygame.mixer.Sound('Sounds/wing.wav')
 
-pockets = [(55, 63), (592, 48), (1134, 64), (55, 616), (592, 629), (1134, 616)]
 
-cushions = [
-    [(88, 56), (109, 77), (555, 77), (564, 56)],
-    [(621, 56), (630, 77), (1081, 77), (1102, 56)],
-    [(89, 621), (110, 600), (556, 600), (564, 621)],
-    [(622, 621), (630, 600), (1081, 600), (1102, 621)],
-    [(56, 96), (77, 117), (77, 560), (56, 581)],
-    [(1143, 96), (1122, 117), (1122, 560), (1143, 581)]
-]
+pipe_group = pygame.sprite.Group()
+base = Base(win)
+score_img = Score(WIDTH // 2, 50, win)
+grumpy = Birds(win)
 
-def create_cushion(poly_dims):
-    body = pymunk.Body(body_type=pymunk.Body.STATIC)
-    body.position = (0, 0)
-    shape = pymunk.Poly(body, poly_dims)
-    shape.elasticity = 0.8
 
-    space.add(body, shape)
+base_height = 0.80 * HEIGHT
+speed = 0
+game_started = False
+game_over = False
+restart = False
+score = 0
+start_screen = True
+pipe_pass = False
+pipe_frequency = 1600
+last_pipe = pygame.time.get_ticks()
 
-for c in cushions:
-    create_cushion(c)
 
-class Cue():
-    def __init__(self, pos):
-        self.original_image = cue_image
-        self.angle = 0
-        self.image = pygame.transform.rotate(self.original_image, self.angle)
-        self.rect = self.image.get_rect()
-        self.rect.center = pos
 
-    def update(self, angle):
-        self.angle = angle
+running = True
+while running:
+    win.blit(bg, (0, 0))
 
-    def draw(self, surface):
-        self.image = pygame.transform.rotate(self.original_image, self.angle)
-        surface.blit(self.image, (self.rect.centerx - self.image.get_width() / 2, self.rect.centery - self.image.get_height() / 2))
+    if start_screen:
+        speed = 0
+        grumpy.draw_flap()
+        base.update(speed)
+        win.blit(flappybird_img, (40, 50))
+    else:
+        if game_started and not game_over:
+            next_pipe = pygame.time.get_ticks()
+            if next_pipe - last_pipe >= pipe_frequency:
+                y = display_height // 2
+                pipe_pos = random.choice(range(-100, 100, 4))
+                height = y + pipe_pos
+                
+                top = Pipe(win, pipe_img, height, 1)
+                bottom = Pipe(win, pipe_img, height, -1)
+                pipe_group.add(top)
+                pipe_group.add(bottom)
+                last_pipe = next_pipe
+        
+        pipe_group.update(speed)
+        base.update(speed)    
+        grumpy.update()
+        score_img.update(score)
+        
+        if pygame.sprite.spritecollide(grumpy, pipe_group, False) or grumpy.rect.top <= 0:
+            game_started = False
+            if grumpy.alive:
+                hit_fx.play()
+                die_fx.play()
+            grumpy.alive = False
+            grumpy.theta = grumpy.vel * -2
+    
+        if grumpy.rect.bottom >= display_height:
+            speed = 0
+            game_over = True
+    
+        if len(pipe_group) > 0:
+            p = pipe_group.sprites()[0]
+            if grumpy.rect.left > p.rect.left and grumpy.rect.right < p.rect.right and not pipe_pass and grumpy.alive:
+                pipe_pass = True
+    
+            if pipe_pass:
+                if grumpy.rect.left > p.rect.right:
+                    pipe_pass = False
+                    score += 1
+                    point_fx.play()
 
-cue = Cue((888, SCREEN_HEIGHT / 2))
-
-power_bar = pygame.Surface((10, 20))
-power_bar.fill(GREEN)
-
-run = True
-while run:
-    clock.tick(FPS)
-    space.step(1 / FPS)
-
-    screen.fill(BG)
-
-    screen.blit(table_image, (0, 0))
-
-    for i, ball in enumerate(balls):
-        for pocket in pockets:
-            ball_x_dist = abs(ball.body.position[0] - pocket[0])
-            ball_y_dist = abs(ball.body.position[1] - pocket[1])
-            ball_dist = math.sqrt((ball_x_dist ** 2) + (ball_y_dist ** 2))
-            if ball_dist <= pocket_dia / 2:
-                if i == len(balls) - 1:
-                    lives -= 1
-                    cue_ball_potted = True
-                    ball.body.position = (-100, -100)
-                    ball.body.velocity = (0.0, 0.0)
-                else:
-                    space.remove(ball.body)
-                    balls.remove(ball)
-                    potted_balls.append(ball_images[i])
-                    ball_images.pop(i)
-
-    for i, ball in enumerate(balls):
-        screen.blit(ball_images[i], (ball.body.position[0] - ball.radius, ball.body.position[1] - ball.radius))
-
-    taking_shot = True
-    for ball in balls:
-        if int(ball.body.velocity[0]) != 0 or int(ball.body.velocity[1]) != 0:
-            taking_shot = False
-
-    if taking_shot and game_running:
-        if cue_ball_potted:
-            balls[-1].body.position = (888, SCREEN_HEIGHT / 2)
-            cue_ball_potted = False
-        mouse_pos = pygame.mouse.get_pos()
-        cue.rect.center = balls[-1].body.position
-        x_dist = balls[-1].body.position[0] - mouse_pos[0]
-        y_dist = -(balls[-1].body.position[1] - mouse_pos[1])
-        cue_angle = math.degrees(math.atan2(y_dist, x_dist))
-        cue.update(cue_angle)
-        cue.draw(screen)
-
-    if powering_up and game_running:
-        force += 100 * force_direction
-        if force >= max_force or force <= 0:
-            force_direction *= -1
-        for b in range(math.ceil(force / 2000)):
-            screen.blit(power_bar, (balls[-1].body.position[0] - 30 + (b * 15), balls[-1].body.position[1] + 30))
-    elif not powering_up and taking_shot:
-        x_impulse = math.cos(math.radians(cue_angle))
-        y_impulse = math.sin(math.radians(cue_angle))
-        balls[-1].body.apply_impulse_at_local_point((force * -x_impulse, force * y_impulse), (0, 0))
-        force = 0
-        force_direction = 1
-
-    pygame.draw.rect(screen, BG, (0, SCREEN_HEIGHT, SCREEN_WIDTH, BOTTOM_PANEL))
-    draw_text("Lives: " + str(lives), font, WHITE, SCREEN_WIDTH - 200, SCREEN_HEIGHT + 10)
-
-    for i, ball in enumerate(potted_balls):
-        screen.blit(ball, (10 + (i * 50), SCREEN_HEIGHT + 10))
-
-    if lives <= 0:
-        draw_text("Game Over", large_font, WHITE, SCREEN_WIDTH / 2 - 160, SCREEN_HEIGHT / 2 - 100)
-        game_running = False
-
-    if len(balls) == 1:
-        draw_text("You Win", large_font, WHITE, SCREEN_WIDTH / 2 - 160, SCREEN_HEIGHT / 2 - 100)
-        game_running = False
-
+                   
+    if not grumpy.alive:
+        win.blit(gameover_img, (50, 200))
+        win.blit(try_again_img, try_again_rect)
+        
     for event in pygame.event.get():
-        if event.type == pygame.MOUSEBUTTONDOWN and taking_shot:
-            powering_up = True
-        if event.type == pygame.MOUSEBUTTONUP and taking_shot:
-            powering_up = False
         if event.type == pygame.QUIT:
-            run = False
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE or event.key == pygame.K_q:
+                running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if start_screen:
+                game_started = True
+                speed = 2
+                start_screen = False
+                game_over = False
+                last_pipe = pygame.time.get_ticks() - pipe_frequency
+                next_pipe = 0
+                pipe_group.empty()
+                speed = 2
+                score = 0
+                
+            if game_over:
+                start_screen = True
+                grumpy = Birds(win)
+                pipe_img = pipe_img
+                bg = bg
+                
+            if not grumpy.alive and try_again_rect.collidepoint(event.pos):
+                start_screen = True
+                grumpy = Birds(win)
+                pipe_group.empty()
+                score = 0
+                game_over = False
 
+    clock.tick(FPS)
     pygame.display.update()
-
 pygame.quit()
